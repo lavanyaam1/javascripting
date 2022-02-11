5d4d12ce274832526a612623d349a0cbc80166eb
lavanyaam1 <99254805+lavanyaam1@users.noreply.github.com>
2022-02-11 23:06:13 +0530

    Update repeateadWords.js

--- repeateadWords.js
+++ repeateadWords.js
@@ -1,32 +1,24 @@
-// 2. Find out if there are repeating words in a given sentence. Make sure to do this in O(n) time.
-// Create a function that takes a sentence as input
+const text = 'It was sunny in the morning but now it is to cold';
 
-let words = "I am busy today";
-
-function chechRepeatedWords(sentence) {
-    let words = sentence.split(" ");
-    let wordMap = {};
-
-    for (let i = 0; i < words.length; i++) {
-        let currentWordCount = wordMap[words[i]];
-        let count = currentWordCount ? currentWordCount : 0;
-        wordMap[words[i]] = count + 1;
-    }
+function chechRepeatingWords(sentence) {
+    sentence=sentence.toLowerCase();
+    const words = sentence.split(' ');
     let flag = 0;
-    for (const key in wordMap) {
-        if(`${wordMap[key]}` > 1){
+    const wordMap = new Map();
+    for (let index = 0; index < words.length; index++) {
+        if (wordMap.has(words[index])) 
+        {
             flag = 1;
-            break;
+        } 
+        else 
+        {
+            wordMap.set(words[index], 1);
         }
     }
-
-    if(flag === 1){
-        console.log("Yes! there are repeating words");
+    if (flag === 1) {
+        console.log("String contains repeating words");
+    } else {
+        console.log("Strings does not contain repeating words");
     }
-    else{
-        console.log("No! repeating words");
-    }
-    return wordMap;
 }
-
-console.log(chechRepeatedWords(words));
+chechRepeatingWords(text);
